75f9804951fe680289d2aad58d2b0fee2db7972d
Brian Young <Brian.Young@sacw.com>
2021-01-22 13:32:55 -0600

    commit initial setup

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,32 @@
 // @TODO: YOUR CODE HERE!
+
+
+
+let svgWidth = window.innerWidth;
+let svgHeight = window.innerHeight;
+
+let margin = {
+    top: 30,
+    right: 60,
+    bottom: 30,
+    left: 60
+};
+
+let width = svgWidth - margin.left - margin.right;
+let height = svgHeight - margin.top - margin.bottom;
+
+let svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+let chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+d3.csv("assets/js/data.csv").then(function(data) {
+
+    console.log(data);
+
+
+});
